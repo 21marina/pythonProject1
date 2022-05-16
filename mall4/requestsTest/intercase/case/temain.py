338e6e7570201553f24022653bc86ec351ec6d9e
ming <862578074@qq.com>
2022-05-16 17:19:18 +0800

    1

--- mall4/requestsTest/intercase/case/temain.py
+++ mall4/requestsTest/intercase/case/temain.py
@@ -5,18 +5,11 @@ import unittest
 import  time
 
 import requests
+from BeautifulReport import BeautifulReport
 from ddt import ddt, data, unpack, file_data
 
-import mall4
-from mall4.pomall4.Page.page_index import pageIndex
-from mall4.pomall4.Page.page_login import pageLogin
-from mall4.pomall4.Page.page_proCart import pageProCart
-from mall4.pomall4.Page.page_proDetails import pageProDetails
-from mall4.pomall4.Base.base import base
-from mall4.pomall4.Page.page_proOrder import pageOrder
-from mall4.pomall4.tools.BeautifulReport import BeautifulReport
-from mall4.pomall4.tools.getLogger import getLogger
-from mall4.pomall4.tools.get_driver import getDriver
+
+
 import urllib
 
 
